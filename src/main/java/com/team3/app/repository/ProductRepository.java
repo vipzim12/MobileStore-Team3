8829a467e568648a928ba8cecac4c71fad23981b
Van Sau <hatcaixanh123@gmail.com>
2018-08-30 00:28:20 +0700

    ss

--- src/main/java/com/team3/app/repository/ProductRepository.java
+++ src/main/java/com/team3/app/repository/ProductRepository.java
@@ -13,13 +13,19 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	@Query(value = "select * from Products p where p.promotion is not null", nativeQuery = true)
 	List<Product> getAllProductSale();
 
-	@Query(value = "select Top(8) * from Products p, Category c, Group_Categories gc where p.id_category = c.id_category and c.id_group_category = gc.id_group_category and c.id_group_category = ?1", 
-			nativeQuery = true)
+	@Query(value = "select Top(8) * from Products p, Category c, Group_Categories gc where p.id_category = c.id_category and c.id_group_category = gc.id_group_category and c.id_group_category = ?1", nativeQuery = true)
 	List<Product> gellProByGCategoryId(Integer id);
-	
-	@Query(value="SELECT P FROM Product P ORDER BY P.price ")
+
+	@Query(value = "SELECT P FROM Product P ORDER BY P.price ")
 	List<Product> getProductSortedAsc();
-	
-	@Query(value="SELECT P FROM Product P ORDER BY P.price DESC")
-  List<Product> getProductSortedDesc();
+
+	@Query(value = "SELECT P FROM Product P ORDER BY P.price DESC")
+	List<Product> getProductSortedDesc();
+
+	@Query(value = "select P.* " + "FROM Products P  JOIN Category  ON Category.id_category=P.id_category "
+			+ "INNER JOIN Group_Categories ON Category.id_group_category= Group_Categories.id_group_category "
+			+ "WHERE Group_Categories.id_group_category " + "in (SELECT Group_Categories.id_group_category "
+			+ "FROM Group_Categories JOIN Category ON Category.id_group_category= Group_Categories.id_group_category "
+			+ "Where Category.id_category=?1 )", nativeQuery = true)
+	List<Product> getProductsGCategoryByIdCategory(int id);
 }
